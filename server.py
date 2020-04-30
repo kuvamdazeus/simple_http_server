eb4d0e71f214f13472668972e1ee7c2fd3ded0f6
Kuvam Bhardwaj <52134060+kuvamdazeus@users.noreply.github.com>
2020-04-30 16:18:52 +0530

    major updates, added a handsome script to the home.html and made changes in server.py script to make it more understandable, tells the ip addersses of the users with a generated website but if the user is using windows, it tells the website in the terms of localhost:8080/home.html

--- server.py
+++ server.py
@@ -1,7 +1,23 @@
+#! /usr/bin/env python3
+import subprocess as sb
 import http.server
 import socketserver
 
-handler = http.server.SimpleRequestHandler
-tcp_server = socketserver.TCPServer(("", 8000), handler)
+default_port = 8080
+handler = http.server.SimpleHTTPRequestHandler
+tcp_server = socketserver.TCPServer(("", default_port), handler)
 
-tcp_server.server_forever()
+def get_ip_address():
+    try:
+        result = sb.run(["ipconfig", "getifaddr", "en0"], capture_output = True)
+        return result.stdout.decode().strip()
+    except Exception:
+        return "localhost:8080/home.html"
+
+def main():
+    print("Website: http://{}:{}/home.html".format(get_ip_address(), default_port))
+    print("Server active")
+    tcp_server.serve_forever()
+
+if __name__ == "__main__":
+    main()
