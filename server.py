b549bfe74b03c67a411f46243e2a7c49762fe6aa
Kuvam Bhardwaj <kuvamyash@gmail.com>
2020-05-25 16:32:55 +0530

    Added some check systems to ensure in case of any error, the user is seeing the correct and is easy to fix for them also

--- server.py
+++ server.py
@@ -5,7 +5,6 @@ import http.server, sys, socketserver, socket, re
 args = sys.argv
 args.pop(0)
 args = " ".join(args)
-print(args)
 port_pattern, address_pattern = r"([0-9]+)", r"(^[a-zA-Z][0-9a-zA-Z\.\-_]+)"
 # searching for patterns in args (argv) list
 server_address = re.search(address_pattern, args)
@@ -18,14 +17,17 @@ if server_address != None:
     address = server_address.group(1)
 if port != None:
     default_port = int(port.group(1).strip())
-print(address, default_port)
+print("Listening on port", default_port)
 # defining a simple HTTP request handler, handles requests over HTTP
 handler = http.server.SimpleHTTPRequestHandler
 try:
-    tcp_server = socketserver.TCPServer((address, default_port), handler) # deploying server
+    tcp_server = socketserver.TCPServer((address, default_port), handler, bind_and_activate=True) # deploying server
 except Exception as error:
-    print("\nServer failed to start, please ensure that the website is not opened on your machine or any other, if so, close the tabs and try again\n")
-    print("Error message:", error)
+    message = str(error)
+    if "already in use" in message:
+        print("\nServer failed to start, please ensure that the website is not opened on your machine or any other, if so, close the tabs and try again\n")
+    else:
+        print("Error message:", message)
     sys.exit(1)
 
 def get_ip_address():
@@ -35,8 +37,8 @@ def get_ip_address():
 def main(): # main function does all required things to bring our new tcp_server on track and running
     print("Website: http://{}:{}/".format(get_ip_address(), default_port))
     print("Server active")
-    tcp_server.serve_forever() # runs the server
-
+    serve_forever = tcp_server.serve_forever() # runs the server
+    print(serve_forever)
 if __name__ == "__main__":
     try:
         main()
